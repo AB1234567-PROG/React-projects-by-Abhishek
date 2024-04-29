e168db5c789da02c32f23cae003c5542e4e4e0ff
Abhishek Singh Chaudhary <79990244+AB1234567-PROG@users.noreply.github.com>
2024-04-29 11:59:07 +0530

    Weather-Forecast-App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-
-import Board from './board';
-
+import Card from './Components/card';
+import Input from './Components/input';
+import Button from './Components/button';
+import { useWeather } from "./Context/Weather";
 
 function App() {
+
+  const weather = useWeather();
+  console.log(weather);
+  
+  useEffect(()=>{
+    weather.fetchCurrentUserLocationData();
+  },[]);
+ 
   return (
+    
     <div className="App">
-      <Board/>
+      <h4>Weather Forecast</h4>
+      <Input/>
+      <Button onClick={weather.fetchData} value = "Search"/>
+      <Card/>
+      <Button onClick={weather.fetchCurrentUserLocationData} value = "Refresh"/>
     </div>
-  );
+  
+ );
 }
 
 export default App;
